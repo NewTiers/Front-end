c5632e2f1236ee52c9c37be346e802a18d358936
NewTiers <133969369+NewTiers@users.noreply.github.com>
2023-07-13 09:39:16 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+	</div>
+
+</div>
+
+<footer>
+	<div class="wrap">
+		<div class="pd10">
+			<div class="footer pd10">
+				Made by &copy; Rio
+			</div>
+		</div>
+	</div>
+</footer>
+
+</body>
+</html>
